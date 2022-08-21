cc66934d12214dcab73a4b4f4fe577e7603ecba7
AlekseyBrovko <96891019+AlekseyBrovko@users.noreply.github.com>
2022-08-21 14:13:53 +0300

    тупик

---
+++ RaceGame/Assets/Scripts/ImphenziaTest/Player.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public enum ControlType { HumanInput, AI}
+    public ControlType controlType = ControlType.HumanInput;
+
+    
+}
